5c318f9f19e5f11a78b7277dae8c69f85a01d40a
apuvvala <ajay.puvvala@mathworks.com>
2017-06-17 08:24:16 -0400

    Running just a few handful of tests

--- runMyTests.m
+++ runMyTests.m
@@ -6,12 +6,12 @@ import matlab.unittest.plugins.ToFile
 jenkins_workspace = getenv('WORKSPACE');
 try
     % Pick up local tests
-    suite1 = testsuite();
-    suite2 = testsuite(fullfile(matlabroot, 'test', 'toolbox', 'stm', 'matlabunit', 'testdata', 'testfolder'));
-    suite3 = testsuite(fullfile(matlabroot, 'test', 'toolbox', 'stm', 'matlabunit', 'testdata', 'testfolder', 'iterations'));
-    suite4 = testsuite(fullfile(matlabroot, 'test', 'toolbox', 'stm', 'matlabunit', 'testdata', 'testfolder', 'tags'));
+    suite = testsuite();
+    %suite2 = testsuite(fullfile(matlabroot, 'test', 'toolbox', 'stm', 'matlabunit', 'testdata', 'testfolder'));
+    %suite3 = testsuite(fullfile(matlabroot, 'test', 'toolbox', 'stm', 'matlabunit', 'testdata', 'testfolder', 'iterations'));
+    %suite4 = testsuite(fullfile(matlabroot, 'test', 'toolbox', 'stm', 'matlabunit', 'testdata', 'testfolder', 'tags'));
     
-    suite = [suite1 suite2 suite3 suite4];
+    %suite = [suite1 suite2 suite3 suite4];
     
     xmlResultsFile = fullfile(jenkins_workspace, 'JUnitResults.xml');
     tapResultsFile = fullfile(jenkins_workspace, 'TAPResults.tap');
