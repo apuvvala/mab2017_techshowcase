33dffa8cc60e6d485addeb4c3d3635feed4b23c4
apuvvala <ajay.puvvala@mathworks.com>
2017-06-17 09:17:25 -0400

    Adding MATLAB Unit Test report

--- runMyTests.m
+++ runMyTests.m
@@ -2,6 +2,7 @@ import matlab.unittest.TestRunner
 import matlab.unittest.plugins.XMLPlugin
 import matlab.unittest.plugins.TAPPlugin
 import matlab.unittest.plugins.ToFile
+import matlab.unittest.plugins.TestReportPlugin
 
 jenkins_workspace = getenv('WORKSPACE');
 try
@@ -15,13 +16,26 @@ try
     
     xmlResultsFile = fullfile(jenkins_workspace, 'JUnitResults.xml');
     tapResultsFile = fullfile(jenkins_workspace, 'TAPResults.tap');
-    reportFile = fullfile(jenkins_workspace, 'TestReport.pdf');
+    %reportFile = fullfile(jenkins_workspace, 'TestReport.pdf');
     
-    runner = TestRunner.withTextOutput;
+    % Create and configure the runner
+    runner = TestRunner.withTextOutput('Verbosity',3);
+    artifactsFolder = fullfile(jenkins_workspace, 'artifacts');
+    mkdir(artifactsFolder);
+    runner.ArtifactsRootFolder = artifactsFolder;    
     runner.addPlugin(TAPPlugin.producingVersion13(ToFile(tapResultsFile)));
     runner.addPlugin(XMLPlugin.producingJUnitFormat(xmlResultsFile));
-    runner.addPlugin(TestManagerReportPlugin(reportFile));
+    %runner.addPlugin(TestManagerReportPlugin(reportFile));
     
+    % Add the MATLAB Unit TestReportPlugin
+    % pdf
+    pdfFile = fullfile(jenkins_workspace, 'TestReport.pdf');
+    runner.addPlugin(TestReportPlugin.producingPDF(pdfFile));
+    
+    % html
+    htmlFolder = fullfile(jenkins_workspace, 'testresults');
+    runner.addPlugin(TestReportPlugin.producingHTML(htmlFolder,...
+        'IncludingCommandWindowText', true,  'IncludingPassingDiagnostics', true));    
     results = runner.run(suite);
 catch e
     disp(getReport(e, 'extended'));
